8fa7d3a9e7ad8e898380728ac88db6159e5df928
M Gil Valverde <m.gilvalverde@gmail.com>
2024-08-22 17:30:37 +0200

    refactor[aws-ssm]: remove parameter is_list - redundancy with stringlist

--- omegaconf_cloud_resolvers/resolvers/aws/parameterstore.py
+++ omegaconf_cloud_resolvers/resolvers/aws/parameterstore.py
@@ -7,28 +7,27 @@ class AWSParameterStoreMixin(AWSMixin):
         return self._session.client('ssm')
 
 class AWSParameterStoreResolver(Resolver, AWSParameterStoreMixin):
-    def __init__(self, session=None, decrypt=True, is_list=True, enforce_numerical=True, *args, **kwargs):
+    def __init__(self, session=None, decrypt=True, infere_types=False, *args, **kwargs):
         super().__init__(session, *args, **kwargs)
         self._decrypt = decrypt
-        self._is_list = is_list
-        self._enforce_numerical = enforce_numerical
+        self._infere_types = infere_types
 
     def __call__(self, name):
         value = self.client.get_parameter(Name=name, WithDecryption=self._decrypt)
         ptype = value["Parameter"]["Type"]
         pvalue = value["Parameter"]["Value"]
-        return self.parse_value(pvalue, ptype, self._decrypt, self._is_list, self._enforce_numerical)
+        return self.parse_value(pvalue, ptype, self._decrypt, self._infere_types)
 
     @staticmethod
-    def parse_value(value, type_, decrypt, is_list, enforce_numerical):
-        if (type_ == 'StringList') and is_list and not decrypt:
+    def parse_value(value, type_, decrypt, infere_types):
+        if (type_ == 'StringList') and not decrypt:
             values = value.split(",")
-            if enforce_numerical:
+            if infere_types:
                 return [try_cast_to_number(v) for v in values]
             else:
                 return values
         else:
-            if enforce_numerical:
+            if infere_types:
                 return try_cast_to_number(value)
             else:
                 return value
