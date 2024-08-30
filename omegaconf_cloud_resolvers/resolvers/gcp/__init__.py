633cbac0e512752bec8e8aa4d99da88912098b23
M Gil Valverde <m.gilvalverde@gmail.com>
2024-08-30 12:35:38 +0200

    ref: refactor how extra dependencies are checked and fix pre-commit

--- omegaconf_cloud_resolvers/resolvers/gcp/__init__.py
+++ omegaconf_cloud_resolvers/resolvers/gcp/__init__.py
@@ -1,3 +1,10 @@
+from importlib.util import find_spec
+
+if not find_spec("google.auth"):
+    raise ImportError(
+        "To use the GCP resolvers, you need to: `pip install omegaconf_cloud_resolvers[gcp]`"
+    )
+
 from .secretmanager import GCPSecretManagerResolver
 
 __all__ = [
